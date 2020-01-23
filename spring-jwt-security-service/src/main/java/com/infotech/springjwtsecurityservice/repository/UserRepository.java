0782b4baedf4abee6284e52efd9ea6ef215e623e
vikaspunde <31177773+vikaspunde@users.noreply.github.com>
2020-01-23 15:46:08 +0530

    poc

---
+++ spring-jwt-security-service/src/main/java/com/infotech/springjwtsecurityservice/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.infotech.springjwtsecurityservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.infotech.springjwtsecurityservice.entity.User;
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer>{
+	
+	public User findByUserName(String username);
+}
---
+++ spring-jwt-security-service/src/test/java/com/infotech/springjwtsecurityservice/SpringJwtSecurityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infotech.springjwtsecurityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtSecurityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
